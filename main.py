cb21fb4c773185b227c8715477df10132ee03bb7
Ibrahim Sha <ibrahimsha.b@s2t.ai>
2023-04-07 13:57:48 +0530

    fix: none type data

--- main.py
+++ main.py
@@ -36,35 +36,37 @@ def get_data(city: str, nct_id: str, PHPSESSID: str) -> List[dict]:
         
         response = response.json()
         json_data["nextdocid"] = response.get("nextdocid")
+        results = response.get("results")
+        if results:
+            data = results.get("data")
+            if data:
+                for item in response.get("results").get("data"):
+                    if len(item) and item[0] != 'null':
+                        pprint.pprint(item)
+                        tmp = {}
+                        tmp["name"] = item[1]
+                        tmp["address"] = ",".join(item[3:5])
+                        tmp["phone"] = "Cant get phone number"
+                        time.sleep(1)
+                        if item[15] == "":
+                            json_data2["allocateid"] = item[49]
+                            response = requests.post(
+                                "https://www.justdial.com/api/callallocate",
+                                params=params2,
+                                cookies=cookies2,
+                                headers=headers2,
+                                json=json_data2,
+                                timeout=10
+                            )
+                            if response.status_code == 200:
+                                data = response.json().get("result")
+                                if data.get("status"):
+                                    tmp["phone"] = data.get("vn")
 
-        if response.get("results"):
-            for item in response.get("results").get("data"):
-                if len(item) and item[0] != 'null':
-                    pprint.pprint(item)
-                    tmp = {}
-                    tmp["name"] = item[1]
-                    tmp["address"] = ",".join(item[3:5])
-                    tmp["phone"] = "Cant get phone number"
-                    time.sleep(1)
-                    if item[15] == "":
-                        json_data2["allocateid"] = item[49]
-                        response = requests.post(
-                            "https://www.justdial.com/api/callallocate",
-                            params=params2,
-                            cookies=cookies2,
-                            headers=headers2,
-                            json=json_data2,
-                            timeout=10
-                        )
-                        if response.status_code == 200:
-                            data = response.json().get("result")
-                            if data.get("status"):
-                                tmp["phone"] = data.get("vn")
-
-                    else:
-                        tmp["phone"] = item[15]
-                    # print(tmp)
-                    output.append(tmp)
+                        else:
+                            tmp["phone"] = item[15]
+                        # print(tmp)
+                        output.append(tmp)
 
     # Convert to csv
     file_name  = f"{city}_{nct_id}.csv"
